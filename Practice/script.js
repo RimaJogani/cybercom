2ebd3102702faa5b1c3dac911197b84d5627a6d7
RimaJogani <rimapatel199808@gmail.com>
2021-01-08 15:36:23 +0530

    basic js perform day1

---
+++ Practice/script.js
@@ -0,0 +1,100 @@
+console.log('Hello World!');
+
+var studentFname = 'Rima'
+console.log(studentFname);
+
+var age = 23;
+
+var fullAge=true;
+console.log(fullAge);
+
+var standard;
+console.log(standard);
+
+standard="5th Year"
+console.log(standard);
+
+//variable cocrcion....
+
+console.log(studentFname + 'is Studing in ' + standard);
+
+//variable mutation....
+
+age = "Twenty Three";
+
+alert(studentFname + ' ' + age);
+
+
+var studentLname=prompt('What is her Last Name?');
+
+console.log(studentFname+' '+studentLname);
+
+
+//basic operators...
+var yearRima,yearRiddhi;
+now=2021;
+
+//math operators(+,-,/,*)
+
+yearRima=now-22;
+yearRiddhi=now-18;
+
+console.log(yearRima);
+console.log(now+20);
+console.log(now*2);
+console.log(now/10);
+
+//logical operators(<,>)
+
+console.log(yearRima > yearRiddhi);
+console.log(yearRima < yearRiddhi);
+
+//typeof operators
+
+console.log(typeof yearRima);
+
+
+//operatore precedence..
+
+
+var ageFull = now - yearRima >= yearRiddhi;//false;
+console.log(ageFull);
+
+var avrage=(yearRima + yearRiddhi)/2;//2001;
+console.log(avrage);
+
+//multiple assignment....
+
+var x,y;
+x=(3+5)*4-6;//8*4-6//32-6//26;
+console.log(x);
+y=x=(3+5)*4-6;//8*4-6//32-6//26;
+console.log(x,y);
+
+//more operators...
+
+x+=2;//x=x+2;
+console.log(x);
+
+x++;
+console.log(x);
+
+x--;
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
